7c8587aebdea68cec241ecf23ee3dcbb69d1eef7
Rodrigo <rodrigoml2395@gmail.com>
2023-05-02 16:53:19 -0300

    feat: user migration

---
+++ back-end/src/database/migrations/20230502195221-create-users.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  }),
+  down: (queryInterface, _Sequelize) => queryInterface.dropTable('users'),
+};
