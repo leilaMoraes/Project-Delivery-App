e4ac7725876396f175d6b793d3976865301d89cd
Leila Moraes <leila.violino@gmail.com>
2023-05-05 16:47:00 -0300

    refactor: page Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
+import Input from '../components/Input';
 
 function Login() {
   const [inputEmail, setInputEmail] = useState('');
@@ -17,41 +19,33 @@ function Login() {
 
   return (
     <div>
-      <label htmlFor="inputEmail">
-        Email
-        <input
-          type="text"
-          name="inputEmail"
-          id="inputEmail"
-          data-testid="common_login__input-email"
-          onChange={ ({ target }) => setInputEmail(target.value) }
-        />
-      </label>
-      <label htmlFor="inputPassword">
-        Senha
-        <input
-          type="password"
-          name="inputPassword"
-          id="inputPassword"
-          data-testid="common_login__input-password"
-          onChange={ ({ target }) => setPassWord(target.value) }
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="common_login__button-login"
+      <Input
+        label="Email"
+        type="text"
+        inputName="inputEmail"
+        id="inputEmail"
+        dataName="common_login__input-email"
+        onChange={ ({ target }) => setInputEmail(target.value) }
+      />
+      <Input
+        label="Senha"
+        type="password"
+        inputName="inputPassword"
+        id="inputPassword"
+        dataName="common_login__input-password"
+        onChange={ ({ target }) => setPassWord(target.value) }
+      />
+      <Button
+        dataName="common_login__button-login"
         disabled={ !(inputEmail.match(/\S+@\S+\.\S+/i)) || (inputPassword.length < magicNumber) }
         onClick={ onClickLogin }
-      >
-        LOGIN
-      </button>
-      <button
-        type="button"
-        data-testid="common_login__button-register"
+        btnName="LOGIN"
+      />
+      <Button
+        dataName="common_login__button-register"
         onClick={ onClickRegister }
-      >
-        Ainda não tenho conta
-      </button>
+        btnName="Ainda não tenho conta"
+      />
       <p
         data-testid="common_login__element-invalid-email"
       />
