06a873e42e9f534513abcb00c1144c9e32c1532d
Leila Moraes <leila.violino@gmail.com>
2023-05-05 16:46:18 -0300

    refactor: page Register

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,42 +1,36 @@
 import React from 'react';
+import Button from '../components/Button';
+import Input from '../components/Input';
 
 function Register() {
   return (
     <div>
       <h1>Register</h1>
-      <label htmlFor="inputName">
-        Nome
-        <input
-          type="text"
-          name="inputName"
-          id="inputName"
-          data-testid="common_register__input-name"
-        />
-      </label>
-      <label htmlFor="inputEmail">
-        Email
-        <input
-          type="text"
-          name="inputEmail"
-          id="inputEmail"
-          data-testid="common_register__input-email"
-        />
-      </label>
-      <label htmlFor="inputPassword">
-        Senha
-        <input
-          type="password"
-          name="inputPassword"
-          id="inputPassword"
-          data-testid="common_register__input-password"
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="common_register__button-register"
-      >
-        CADASTRAR
-      </button>
+      <Input
+        label="Nome"
+        type="text"
+        inputName="inputName"
+        id="inputName"
+        dataName="common_register__input-name"
+      />
+      <Input
+        label="Email"
+        type="text"
+        inputName="inputEmail"
+        id="inputEmail"
+        dataName="common_register__input-email"
+      />
+      <Input
+        label="Senha"
+        type="password"
+        inputName="inputPassword"
+        id="inputPassword"
+        dataName="common_register__input-password"
+      />
+      <Button
+        dataName="common_register__button-register"
+        btnName="CADASTRAR"
+      />
       <p
         data-testid="common_register__element-invalid_register"
       />
