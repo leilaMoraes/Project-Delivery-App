6757b69d8d5c264354db724053f65a83d9ddfd4c
Leila Moraes <leila.violino@gmail.com>
2023-05-05 16:41:51 -0300

    refactor: create component Button

---
+++ front-end/src/components/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+  const { btnClass, dataName, id, disabled, onClick, btnName } = props;
+  return (
+    <button
+      className={ `btn-${btnClass}` }
+      type="button"
+      data-testid={ dataName }
+      id={ id }
+      disabled={ disabled }
+      onClick={ onClick }
+    >
+      {btnName}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  btnClass: PropTypes.string,
+  dataName: PropTypes.string,
+  id: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  btnName: PropTypes.node,
+};
+
+Button.defaultProps = {
+  btnClass: '',
+  dataName: '',
+  id: '',
+  disabled: false,
+  onClick: () => {},
+  btnName: null,
+};
+
+export default Button;
