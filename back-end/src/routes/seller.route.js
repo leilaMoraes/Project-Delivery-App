d1842bf97eb67f2fb4deb4fe1766a412f4f957c2
Rodrigo <rodrigoml2395@gmail.com>
2023-05-07 19:56:50 -0300

    fix: remove middleware from seller

--- back-end/src/routes/seller.route.js
+++ back-end/src/routes/seller.route.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const { sellerController } = require('../controllers');
 const auth = require('../middlewares/auth');
-const { validateSeller } = require('../middlewares/validateRole');
 
 const sellerRouter = express.Router();
 
 sellerRouter.use(auth);
-
-sellerRouter.use(validateSeller);
 
 sellerRouter.get('/', sellerController.getAll);
 
